33d41bcfe19b2d7d7e9315f46c3f7e0ab915aeb8
Sakshi Sahu <sakshisahu6999@gmail.com>
2026-02-17 12:25:14 +0530

    Implemented RDB checksum protection

--- rdb.go
+++ rdb.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"encoding/gob"
+	"encoding/hex"
+	"io"
 	"log"
 	"os"
 	"path"
@@ -50,8 +54,8 @@ func IncrRDBTrackers() {
 }
 
 func SaveRDB(state *AppState){
-	fp := path.Join(state.conf.dir, state.conf.rdbFn)
-	f, err := os.OpenFile(fp, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	fp := path.Join(state.conf.dir, state.conf.rdbFn) //WRONLY -> RDWR
+	f, err := os.OpenFile(fp, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
 	if err != nil {
 		log.Println("Error opening RDB file:", err)
 		return
@@ -59,20 +63,53 @@ func SaveRDB(state *AppState){
 	defer f.Close()
 
 	log.Println("saving DB to RDB file")
+	var buf bytes.Buffer
 	if state.bgSaveRunning {
-		err = gob.NewEncoder(f).Encode(&state.dbCopy)
-		return
+		err = gob.NewEncoder(&buf).Encode(&state.dbCopy)
 	} else {
 		DB.mu.RLock()
-		err = gob.NewEncoder(f).Encode(&DB.store)
+		err = gob.NewEncoder(&buf).Encode(&DB.store)
 		DB.mu.RUnlock()
 	}
-	
 
 	if err != nil {
-		log.Println("Error saving RDB file:", err)
+		log.Println("Error encoding db:", err)
 		return
 	}
+
+	data := buf.Bytes()
+
+	bsum, err := Hash(&buf)
+	if err != nil {
+		log.Println("rdb - cannot compute buf checksum:", err)
+		return
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		log.Println("rdb - cannot write to file:", err)
+		return
+	}
+	if err := f.Sync(); err != nil {
+		log.Println("rdb - cannot flush file to disk:", err)
+		return
+	}
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		log.Println("rdb - cannot seek file: ", err)
+		return
+	}
+
+	fsum, err := Hash(f)
+	if err != nil {
+		log.Println("rdb - cannot compute file checksum:", err)
+		return
+	}
+
+	if bsum != fsum {
+		log.Printf("rdb - buf and file checksums do not match:\nf=%s\nb=%s\n", fsum, bsum)
+		return
+	}
+
 	log.Println("saved RDB file")
 }
 
@@ -93,3 +130,11 @@ func SyncRDB(conf *Config) {
 	}
 	log.Println("synced RDB file")
 }
+
+func Hash(r io.Reader) (string, error) {
+	h := sha256.New()
+	if _, err := io.Copy(h, r); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(h.Sum(nil)), nil
+}	
